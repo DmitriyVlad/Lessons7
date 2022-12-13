5e83518cc1055012577927e6807947ae49046148
Dmitriy <timon@yahoo.com>
2022-12-13 13:58:43 +0300

    Задание 47

---
+++ work7_47/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 47 Создание двумерного массива, заполненного вещественными числами
+
+double[,] matrix = new double[3, 4];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double a = new Random().Next(-10, 10);
+        double b = new Random().NextDouble();
+        matrix[i, j] = Math.Round(a*b, 1);
+        Console.Write($"{matrix[i, j]}  ");
+    }
+    Console.WriteLine();
+}
